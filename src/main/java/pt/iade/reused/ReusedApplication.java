6137750ad103817debee24d4318329f5c4ae1a3f
Joao-A-Aguas <50039634@europeia.pt>
2021-01-03 21:53:26 +0000

    ola primeiro

---
+++ src/main/java/pt/iade/reused/ReusedApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.reused;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReusedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReusedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/iade/reused/ReusedApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.reused;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReusedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
